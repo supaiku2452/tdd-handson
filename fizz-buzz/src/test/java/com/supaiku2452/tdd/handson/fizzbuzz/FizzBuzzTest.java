26fffbe3452dd6a1f66db65ae31b9ddd912f5177
supaiku2452 <supaiku2452@gmail.com>
2018-07-22 15:24:04 +0900

    tdd first step.

---
+++ fizz-buzz/src/test/java/com/supaiku2452/tdd/handson/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.supaiku2452.tdd.handson.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void test1toOne() {
+        String actual = "1";
+        assertEquals("1", actual);
+    }
+}
