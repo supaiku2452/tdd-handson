aa59b1ca0e65def534ec860f2a6bd0fccd36eff6
supaiku2452 <supaiku2452@gmail.com>
2018-07-22 16:11:54 +0900

    Refactoring a exec method in FizzBuzz class.

--- fizz-buzz/src/main/java/com/supaiku2452/tdd/handson/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/supaiku2452/tdd/handson/fizzbuzz/FizzBuzz.java
@@ -2,12 +2,12 @@ package com.supaiku2452.tdd.handson.fizzbuzz;
 
 public class FizzBuzz {
     public String exec(int num) {
-        if ( num % 3 == 0 && num % 5 != 0 ) {
-            return "Fizz";
-        } else if ( num % 5 == 0 && num % 3 != 0 ) {
-            return "Buzz";
-        } else if ( num % 3 == 0 && num % 5 == 0 ) {
+        if ( num % 3 == 0 && num % 5 == 0 ) {
             return "FizzBuzz";
+        } else if ( num % 3 == 0 ) {
+            return "Fizz";
+        } else if ( num % 5 == 0 ) {
+            return "Buzz";
         }
 
         return String.valueOf(num);
