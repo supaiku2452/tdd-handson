ad5e682bef4b1777bcd62fa3b8ede44f14c91163
supaiku2452 <supaiku2452@gmail.com>
2018-07-22 22:29:21 +0900

    [LRU Cache] refactoring test and method.

--- lru-cache/src/main/java/com/supaiku2452/tdd/handson/lru_cache/LruCache.java
+++ lru-cache/src/main/java/com/supaiku2452/tdd/handson/lru_cache/LruCache.java
@@ -1,11 +1,17 @@
 package com.supaiku2452.tdd.handson.lru_cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LruCache {
-    public boolean add(String a, String data1) {
+    Map<String, String> lruCacheMap = new HashMap<>();
+
+    public boolean add(String key, String value) {
+        lruCacheMap.put(key, value);
         return true;
     }
 
-    public String get(String a) {
-        return "data1";
+    public String get(String key) {
+        return lruCacheMap.get(key);
     }
 }
--- lru-cache/src/test/java/com/supaiku2452/tdd/handson/lru_cache/TestLruCache.java
+++ lru-cache/src/test/java/com/supaiku2452/tdd/handson/lru_cache/TestLruCache.java
@@ -3,6 +3,8 @@ package com.supaiku2452.tdd.handson.lru_cache;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -10,8 +12,16 @@ public class TestLruCache {
 
     @Test
     public void testAddLruCache() {
+        String[][] testCacheDatas = {
+                {"a", "data1"},
+                {"b", "data2"},
+        };
+
         LruCache lruCache = new LruCache();
-        assertTrue(lruCache.add("a", "data1"));
-        assertEquals("data1", lruCache.get("a"));
+
+        Arrays.asList(testCacheDatas).stream().forEach(testCacheData -> {
+            assertTrue(lruCache.add(testCacheData[0], testCacheData[1]));
+            assertEquals(testCacheData[1], lruCache.get(testCacheData[0]));
+        });
     }
 }
